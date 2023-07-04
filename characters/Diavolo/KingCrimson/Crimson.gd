b7eda476243c7edb6bd5ef1554eada7eb43630d6
uGuardian <Sean.Heavysege@gmail.com>
2023-07-04 08:52:11 -0600

    Fixed a minor bug in KC's style applicator

--- characters/Diavolo/KingCrimson/Crimson.gd
+++ characters/Diavolo/KingCrimson/Crimson.gd
@@ -18,21 +18,25 @@ func init(pos = null):
 	.init(pos)
 	gravity_enabled = false
 	has_projectile_parry_window = false
-	
+
+	if creator:
+		apply_style()
+
+func apply_style():
 	if Global.enable_custom_colors:
-		
+
 		var style = creator.storedStyle
 		var exColor1 = creator.storedExtraColor1
-		
+
 		if style == null || exColor1 == Color("711311"):
 			return
-		
+
 		var aura1 = $Flip/Particles/Aura/OrbParticles2
 		var aura2 = $Flip/Particles/Aura/OrbParticles3
-		
+
 		var acol1 = Color(creator.storedExtraColor2)
 		var acol2 = Color(creator.storedExtraColor2)
-		
+
 		if aura1 != null && aura2 != null && acol1 != null:
 			acol1.a = aura1.color.a
 			aura1.color = acol1
@@ -42,14 +46,14 @@ func init(pos = null):
 			acol2.g *= 0.6
 			acol2.b *= 0.4
 			aura2.color = acol2
-		
-		
+
+
 		Custom.apply_style_to_material(style, sprite.get_material())
 		sprite.get_material().set_shader_param("extra_color_1", exColor1)
 		sprite.get_material().set_shader_param("extra_replace_color_1", extra_color_1)
 		sprite.get_material().set_shader_param("use_extra_color_1", true)
-		
-		
+
+
 		if style != null && Global.enable_custom_particles and not is_ghost and style.show_aura and style.has("aura_settings"):
 			aura_particle = preload("res://fx/CustomTrailParticle.tscn").instance()
 			particles.add_child(aura_particle)
@@ -60,9 +64,6 @@ func init(pos = null):
 				aura_particle.z_index = - 1
 			else:
 				aura_particle.z_index = 1
-			
-
-
 
 func tick():
 	if hitlag_ticks > 0:
@@ -83,7 +84,7 @@ func hit_by(hitbox):
 	if isBeingHitWhileOwnerNotBeingHit():
 		hitlag_ticks = min(hitlag_ticks, 6)
 		creator.opponent.hitlag_ticks = min(creator.opponent.hitlag_ticks, 6)
-	
+
 	.hit_by(hitbox)
 
 func isBeingHitWhileOwnerNotBeingHit():
@@ -92,32 +93,32 @@ func isBeingHitWhileOwnerNotBeingHit():
 	for hitbox in opp_hitboxes:
 		if hitbox.overlaps(creator.hurtbox):
 			return false
-	
+
 	return true
 
 
 func follow_owner():
 	var targetPos = creator.position
 	targetPos.y = targetPos.y - 30
-	
+
 	targetPos.x += float(creator.desiredStandOffset.x) / 2.0
 	targetPos.y += float(creator.desiredStandOffset.y) / 2.0
-	
+
 	var pos =  get_pos()
 	var difference = Vector2(targetPos.x - pos.x, targetPos.y - pos.y);
-	
+
 	if not attacking:
 		z_index = 0
 		apply_force(str(clamp(difference.x, -MaxFollowForce, MaxFollowForce) * FollowSpeed), str(clamp(difference.y,-MaxFollowForce, MaxFollowForce) * FollowSpeed))
-		var xspeed = float(get_vel().x) 
-		if xspeed == null: 
+		var xspeed = float(get_vel().x)
+		if xspeed == null:
 			xspeed = 0
 		var facingInt = 1
 		if xspeed < 0:
 			facingInt = -1
 		if abs(xspeed) > 5:
 			set_facing(facingInt)
-		
+
 		if abs(xspeed) > FollowAnimThreshold:
 			state_machine._change_state("Follow")
 		elif creator.state_machine.state.name == "Taunt":
@@ -128,7 +129,7 @@ func follow_owner():
 			set_facing(creator.get_facing_int())
 		else:
 			state_machine._change_state("Wait")
-	
+
 	apply_x_fric(Friction)
 	apply_y_fric(Friction)
 
@@ -137,7 +138,7 @@ func get_opponent():
 
 func apply_hitboxes():
 	if creator.erasedFramesLeft > 0 || creator.invulnFramesLeft > 0:
-		return 
+		return
 	.apply_hitboxes()
 
 func copy_to(f):
@@ -147,6 +148,7 @@ func copy_to(f):
 	f.attacking = attacking
 	f.set_facing(get_facing_int())
 	f.memorizedDirection = memorizedDirection
+	apply_style()
 
 func travel_towards_creator():
 	return
